ad59d5ed621b0b0078599f15c80dbbbe519967fb
bookert921 <bookert921@gmail.com>
2020-11-29 19:23:04 -0800

    assoiciates tasks to goal

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,77 +2,60 @@ import React, { useState, useEffect } from 'react'
 import Task from './Task'
 import TaskModel from '../models/task'
 import CreateTask from './CreateTask'
-import GoalList from './GoalList'
 
 const TaskList = (props) => {
    const [taskList, setTaskList] = useState([])
 
-  
-   
    const fetchTasks = async () => {
       const response = await TaskModel.all()
-      const tasks = response.tasks
-      setTaskList(tasks);
-      console.log(tasks[0])
+      const task = await response.tasks
+      setTaskList(task);
    }
 
-   // const createTask = async (task) => {
-   //    const newTask = {
-   //       title: task,
-   //       goalId: props.goal.id,
-   //       completed: false
-   //    }
-   //    await TaskModel.create(newTask)
-   //       .then((res) => {
-   //          console.log('line 43 tasklist.js', res.task) // gets the new task created
-   //          TaskModel.all(res.task)
-   //          tasks.push(res.task)
-   //          fetchTasks()
-   //       })
+   const createTask = async (tasks) => {
+      const newTask = {
+         title: tasks,
+         goalId: props.goal.id,
+         completed: false
+      }
+      await TaskModel.create(newTask)
+         .then(() => {
+            fetchTasks()
+         })
 
-   // }
+   }
 
-   // let allTasks = tasks.map((task, i) => {
-   //    // if (props.goal.id === res.tasks.goalId) {
-      
-   //       return <Task
-   //       key={i}
-   //       task={task}
-   //       fetchTasks={fetchTasks}
-   //       // completed={completedTask}
-   //    />
-   //    // }
-   // })
-
-
-   // const completedTask = (id) =>{
-   //    setTasks(props.tasks.map(checked =>{
-   //       if(checked.id === id){
-   //          checked.completed = !checked.completed
-   //       }
-   //       return checked
-   //    }))
-   // }
-
-   // first pass at this function:
-   // const getTasks = async () => {
-   //    TaskModel.all(currentUser.id)
-   //    const res = await fetch(`URL/${currentUser}`)
-   //    const taskArray = await res.json()
-   //    setTasks(taskArray)
-   // }
    useEffect(() => {
       fetchTasks()
    }, [])
-
+   console.log(taskList[0]);
+   
    return (
-      <div>
-         {taskList.map(task =>{
-               if(task.id === props.goal.id) {
-                  return <p key={task.id}>{task.title}</p>
+      <>
+      <p className="task-form">
+         <button className="btn btn-outline-info btn-sm" type="button" data-toggle="collapse" data-target={`#collapse${props.goal.id}`} aria-expanded="false" aria-controls="collapseExample">
+            See your tasks
+         </button>
+      </p>
+      <div className="collapse" id={`collapse${props.goal.id}`}>
+         <div>
+            {taskList.map(task =>{
+               if(task.goalId == props.goal.id) {
+                  return (
+                     <Task 
+                        key={task.id} 
+                        task={task}
+                        // completedTask={completedTask}
+                     />
+                  )
                }
-         })}
+            })}
+         </div>
       </div>
+      <hr />
+      <CreateTask createTask={createTask} />
+      </>
+
    )
 }
 
